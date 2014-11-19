b8b8f5a5df4522f0d3935bdd7848781557a77c68
ljchang <lukejchang@gmail.com>
2014-11-19 09:36:57 -0700

    changed rolling cv option to tscv.m to include more test samples

--- Matlab/Stats/tscv.m
+++ Matlab/Stats/tscv.m
@@ -14,13 +14,15 @@ function [trIdx, teIdx] = tscv(vectorlen, varargin)
 %
 % Optional inputs with their default values:
 % :--------------------------------------------
-% 'rolling' = [stepsize]    : use rolling cv with the number of
-%                             observations as 'stepsize', test = 1
-%                             observation
+%
 % 'hvblock' = [h,v]         : use hvblock cross-validation with a block
 %                             size of 'h' (0 reduces to v-fold xval)and
 %                             number of test observations 'v' (0 reduces
 %                             to h-block xval)
+%
+% 'rolling' = [h,v,g]       : use hvblock cross-validation with g training steps
+%                             surrounding hv block.  Akin to Rolling
+%                             crossval.  Same properties as hvblock.
 %
 % Outputs:
 % ---------------------------------------------------------------------
@@ -30,6 +32,7 @@ function [trIdx, teIdx] = tscv(vectorlen, varargin)
 % Examples:
 % ---------------------------------------------------------------------
 % [trIdx, teIdx] = tscv(100, 'hvblock',[5,2]); % use hvblock with h=5 and v=2
+% [trIdx, teIdx] = tscv(100, 'rolling',[5,2,10]); % use hvblock with h=5, v=2 and g=10
 %
 % Original version: Copyright Luke Chang & Hedwig Eisenbarth 11/2013
 
@@ -40,6 +43,11 @@ function [trIdx, teIdx] = tscv(vectorlen, varargin)
 %       median > mid
 %       rewrote the hv block so that it loops though all available data -
 %       need to finish coding the rollingcv option
+% LC & HE 12/16/13:
+%       -added rollingcv option
+%
+% HE & LC 11/19/14: 
+%       -increased the test data used by adjusting how the training blocks work at the ends
 
 for i = 1:length(varargin)
     if ischar(varargin{i})
@@ -55,7 +63,10 @@ for i = 1:length(varargin)
                 
             case {'rolling'}
                 xval_type = varargin{i};
-                stepsize = varargin{i + 1};
+                inval = varargin{i + 1};
+                h = inval(1);
+                v = inval(2);
+                g = inval(3);
                 varargin{i} = [];
                 varargin{i + 1} = [];
         end
@@ -74,33 +85,65 @@ switch xval_type
         end
         
         start = 1;
-        stop = stepsize;
         while start <= vectorlen - stepsize + 1
             trIdx{start} = true(vectorlen,1);
             teIdx{start} = false(vectorlen,1);
             trIdx{start}(start:(start + 2*h + 2*v)) = false; %train set = everything - 2*v + 2*h + 1
             teIdx{start}((start + h):(start + h + 2*v)) = true; %test set = 2*v + 1
             start = start + 1;
-            stop = stop + 1;
         end
         
     case 'rolling' % this needs to be fixed.
-        %rolling cross validation: leave completely out h steps around the test interval
+        %rolling cross validation: leave completely out h steps around the
+        %test interval with g training steps
         %See http://robjhyndman.com/hyndsight/tscvexample/ for more info
-        mid = median(1:stepsize);
-        start = 1;
-        stop = stepsize;
-        for k = 1:vectorlen %create training and test for all step sizes, but treat last one differently to account for remainder.
-            trIdx{k} = false(vectorlen,1);
-            teIdx{k} = false(vectorlen,1);
-            trIdx{k}(start:start-1+(mid-v-h)) = true;
-            trIdx{k}(start-1+(mid+h):stop) = true;
-            teIdx{k}(start-1+mid) = true;
-            if rolling == 1
-                start = start + stepsize;
-                stop = stop + stepsize;
-            else
-            end
+        
+        if g == 0
+            error('g must be greater than 0')
         end
+        
+        stepsize = 2*g + 2*v + 2*h + 1;
+        
+        if stepsize > vectorlen;
+            error('stepsize is too large, please decrease')
+        end
+        
+        start = 1;
+        while start <= (vectorlen - (2*v))
+            
+            %Initialize vector as zeros
+            trIdx{start} = false(vectorlen,1);
+            teIdx{start} = false(vectorlen,1);
+            
+            if start <= g+h+1 % adjustment for the g+h first - add points to training on other side of test
+                if start <= h+1
+                    trIdx{start}((start + 2*v+1 + h) : (start + 2*v + h + 2*g)) = true;
+                    teIdx{start}(start:(start + 2*v)) = true; %test set = 2*v + 1
+                else
+                    trIdx{start}((start - (start-1)) : (start - h-1)) = true;
+                    trIdx{start}((start + 2*v+1 + h) : (h + 2*v+1 + h + 2*g)) = true;
+                    teIdx{start}(start:(start + 2*v)) = true; %test set = 2*v + 1
+                end
+                
+            elseif start >= (vectorlen - ((2*v) + g+h)) % adjustment for the g+h last timepoints  - add points to training on other side of test
+                if start > (vectorlen - (2*v + h))
+                    trIdx{start}((start - (2*g + h)) : (start - h - 1)) = true; %(start - (g + h)) : (start - h - 1)
+                    teIdx{start}(start:(start + 2*v)) = true;
+                else
+                    trIdx{start}((vectorlen - stepsize + 1) : (start - h -1)) = true;
+                    trIdx{start}((start + (2*v+1) + h) : vectorlen) = true;
+                    teIdx{start}(start:(start + 2*v)) = true;
+                end
+                
+            else %Normal hvg block
+                trIdx{start}((start- (g+h)):(start - h - 1)) = true; %train set = everything - 2*v + 2*h + 1
+                trIdx{start}((start + (2*v+1) + h): (start + (2*v+1) + h + g -1)) = true; %train set = everything - 2*v + 2*h + 1
+                teIdx{start}(start:(start + 2*v)) = true;
+                %teIdx{start}((start + g + h):(start + g + h + 2*v)) = true; %test set = 2*v + 1
+            end
+            start = start + 1;
+        end
+        
+        
 end
 
