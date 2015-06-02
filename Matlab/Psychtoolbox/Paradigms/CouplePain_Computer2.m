99b4a3cdaa9bb3d75ecb182677ec62bbc99fbfac
Luke Chang <lukejchang@gmail.com>
2015-06-02 16:20:15 -0600

    Bug Fixes

--- Matlab/Psychtoolbox/Paradigms/CouplePain_Computer2.m
+++ Matlab/Psychtoolbox/Paradigms/CouplePain_Computer2.m
@@ -13,7 +13,8 @@
 % Copyright (c) 2014 Luke Chang
 %
 % Permission is hereby granted, free of charge, to any person obtaining a
-% copy of this software and associated documentation files (the "Software"),
+% copy of this software and associated documentation files (theco
+% "Software"),co
 % to deal in the Software without restriction, including without limitation
 % the rights to use, copy, modify, merge, publish, distribute, sublicense,
 % and/or sell copies of the Software, and to permit persons to whom the
